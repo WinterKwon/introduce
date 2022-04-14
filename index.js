e458ea2813c998a4e69c8b5c3da9b71aebafe282
WinterKwon <winterkwon7017@gmail.com>
2022-04-14 22:16:44 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// let enlarge = document.getElementsByClassName("enlarge")
+
+// enlarge.addEventListener("mouseover", function (e){
+//     e.style.fontSize = "14px";
+
+// });
+
+
+$(document).ready(function(){
+
+    $('.enlarge').hover(function() {
+        $('.enlarge').style.transform.
+
+        
+    })
+
+})
