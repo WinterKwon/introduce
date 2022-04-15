7138b3916756f8460d1aaaf15656ac2f5885dc8e
WinterKwon <winterkwon7017@gmail.com>
2022-04-15 16:34:55 +0900

    nav바 이미지, 아이콘 배치. 전면재작성

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-// let enlarge = document.getElementsByClassName("enlarge")
+// let home = document.getElementById("home");
 
-// enlarge.addEventListener("mouseover", function (e){
-//     e.style.fontSize = "14px";
+
+// home.addEventListener("mouseover", function(e){
+//     e.preventDefault();
+
+//     let homeText = document.createElement('p');
+//     homeText.innerText = "home";
+//     this.appendChild(homeText);
 
 // });
 
-
-$(document).ready(function(){
-
-    $('.enlarge').hover(function() {
-        $('.enlarge').style.transform.
-
-        
-    })
-
-})
+/// 우하하 이렇게 하면 마우스오보동안 계속 텍스트가 생김!! css에서 처리하자!
